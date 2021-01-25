271a6be6e614529a7debd06020a1bec47fdca9e9
SolwinInfotech <stdabhoya@yahoo.com>
2021-01-25 13:39:00 +0530

    initial commit

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin Order Success Page Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_OrderSuccessPage
+ * @copyright  Copyright © 2006-2021 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_OrderSuccessPage',
+    __DIR__
+);
